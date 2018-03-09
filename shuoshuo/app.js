f41a35641ba73192780c4bcfd5d51d928dcc55db
LeviLY <15397236229@163.com>
2018-03-09 16:54:56 +0800

    新增说说

--- shuoshuo/app.js
+++ shuoshuo/app.js
@@ -4,9 +4,19 @@
 var express = require("express");
 var app = express();
 var router = require("./router/router.js");
+
+
 app.set("view engine","ejs");
 
 app.use(express.static("./public"));
 
-app.get("/",router.showIndex)
+app.get("/",router.showIndex);
+app.get("/login",router.showLogin);
+app.get("/register",router.showRegister);
+app.get("/userCenter",router.showCenter);
+
+
+
+app.post("/doLogin",router.doLogin);
+app.post("/doRegister",router.doRegister);
 app.listen(3000);
--- shuoshuo/router/router.js
+++ shuoshuo/router/router.js
@@ -1,6 +1,53 @@
 /**
  * Created by Administrator on 2018/3/7 0007.
  */
+var formidable = require("formidable");
+var md5 = require("../models/md5.js");
+var db = require("../models/db.js");
 exports.showIndex = function(req,res,next){
     res.render("index");
+}
+exports.showLogin = function(req,res,next){
+    res.render("login");
+}
+exports.showRegister = function(req,res,next){
+    res.render("register");
+}
+exports.showCenter = function(req,res,next){
+    res.render("userCenter");
+}
+
+
+
+
+exports.doLogin = function(req,res,next){
+
+}
+exports.doRegister = function(req,res,next){
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields) {
+        console.log(fields.username);
+        var username = fields.username,
+            password = fields.password;
+        password = md5(md5(password).substr(4,7) + md5(password));
+        db.find("users",{"username":username},function(err,result){
+            if(result.length==0){
+                db.insertOne("users",{
+                    "username":username,
+                    "password":password,
+                    "time":new Date()
+                }, function (err,result) {
+                    if(err){
+                        res.json({"result":1});
+                        return;
+                    }
+                    res.json({"result":0});
+                })
+            }else{
+                res.json({"result":-1});
+            }
+           //1-插入数据出错，0-成功，-1-用户已存在
+        });
+
+    });
 }
